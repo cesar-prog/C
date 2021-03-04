e00f1c0d5e63a3dfa6a56166aefe0f6ae4fa8165
cesar-prog <78566449+cesar-prog@users.noreply.github.com>
2021-03-03 22:53:41 -0700

    Create homework2.c

---
+++ homework/homework2.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <math.h>
+#define K 150
+
+void prime(int n);
+void binary(int j);
+int main() {
+
+prime(K);
+return 0;
+}
+
+void binary(int j)
+{
+    int i=0;
+    int base[10]={0};
+    int num=j;
+    while(num>0)
+    {
+    base[i]= num%2;
+    num/=2;
+    ++i;
+    }
+    for(int x=6;x>=0;--x)
+    {
+        printf("%3ld",base[x]);
+    }
+    printf("\n");
+
+}
+
+void prime(int k)
+{
+    int count, i, flag, sr;
+    int n=1;
+    count=1;
+
+    while(count <= k){
+
+        sr=sqrt(n);
+        flag = 0;
+        for(i=2; i<= sr; i++){
+            if(n%i==0) {
+                flag = 1;
+                break;
+            }
+
+        }
+        if(flag==0) {
+
+            binary(n);
+            count++;
+        }
+
+        n++;
+    }
+
+}
