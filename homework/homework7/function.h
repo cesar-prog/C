96a38aadf2870a2363398b02b7fb85215887ae55
cesar-prog <78566449+cesar-prog@users.noreply.github.com>
2021-05-06 12:27:45 -0600

    Create function.h

---
+++ homework/homework7/function.h
@@ -0,0 +1,16 @@
+typedef struct {
+  char* name;
+  char* day;
+  char* entry;
+  char* egress;
+  unsigned int pin;
+} employee;
+
+struct employee{
+ char idemp[5];
+ char workDay[20];
+ char start[5];
+ char end[5];
+ 
+};
+struct employee eRegister[200];
