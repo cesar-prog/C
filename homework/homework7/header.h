3b93dcfb43902acbb7f4ce25df905467bb91de9c
cesar-prog <78566449+cesar-prog@users.noreply.github.com>
2021-05-06 13:54:57 -0600

    Create header.h

---
+++ homework/homework7/header.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+int HorasLab(int pin);
+int Registro(void);
+int checkPines(int pin);
+int verAsistencias(int pin);
+int TiempoWork(int pin);
+
+/* Hecho con la idea de Abner y AntonioCG2002*/
+/* no terminada la homework7*/
