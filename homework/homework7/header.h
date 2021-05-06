da80bfadad15c2b3c800244517a231eca9fa71c9
cesar-prog <78566449+cesar-prog@users.noreply.github.com>
2021-05-06 13:34:29 -0600

    Delete header.h

--- homework/homework7/header.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-int HorasLab(int pin);
-int Registro(void);
-int checkPines(int pin);
-int verAsistencias(int pin);
-int TiempoWork(int pin);
