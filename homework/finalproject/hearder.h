ead88224ac4d3ad9a6a53169c13d946b6a2b39a6
cesar-prog <78566449+cesar-prog@users.noreply.github.com>
2021-05-20 11:10:07 -0600

    Create hearder.h

---
+++ homework/finalproject/hearder.h
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <time.h> 
+
+int BookMenu();
+int ShowGarciaMenu();
+int ShowGarciaSubMenu();
+int ShowDashnerMenu();
+int ShowDashnerSubMenu();
+int ShowCoelhoMenu();
+int ShowCoelhoSubMenu();
+int ShowChapmanMenu ();
+int ShowChapmanSubMenu ();
+int temppin, temptime;
+ 
+void PrintOverviewD();
+void PrintPagesD();
+void PrintCheckItD();
+void PrintOverviewG();
+void PrintPagesG();
+void PrintCheckItG();
+void PrintOverviewC();
+void PrintPagesC();
+void PrintCheckItC();
+void PrintOverviewCh ();
+void PrintPagesCh ();
+void PrintCheckItCh ();
